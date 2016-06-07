77c8069b08b310b4f35fab46817fc306d8d7d272
mreis <wmr049@gmail.com>
2016-06-07 06:04:22 -0300

    Cap. 9. Aceitando convites e exibindo nossos contatos

--- perfis/views.py
+++ perfis/views.py
@@ -1,19 +1,28 @@
 from django.shortcuts import render,redirect
-from perfis.models import Perfil
+from perfis.models import Perfil,Convite
 
 def index(request):
 	return render(request, 'index.html',{'perfis' : Perfil.objects.all(), 'perfil_logado' : get_perfil_logado(request)})
 
 def exibir(request,perfil_id):
+	
 	perfil = Perfil.objects.get(id=perfil_id)
 
-	return render(request,'perfil.html', {"perfil" : perfil})
+	perfil_logado = get_perfil_logado(request)
+	ja_eh_contato = perfil in perfil_logado.contatos.all()
+	
+	return render(request,'perfil.html', {"perfil" : perfil, 'ja_eh_contato' : ja_eh_contato})
 
 def convidar(request, perfil_id):
 	perfil_a_convidar = Perfil.objects.get(id=perfil_id)
 	perfi_logado = get_perfil_logado(request)
 	perfi_logado.convidar(perfil_a_convidar)
 
+	return redirect('index')
+
+def aceitar(request, convite_id):
+	convite = Convite.objects.get(id=convite_id)
+	convite.aceitar()
 	return redirect('index')
 
 
